e244afab655da8d099cec9a210d9cb19ec585287
Marcos Bastidas <marcosbastidas301@gmail.com>
2022-01-25 17:37:37 -0400

    update-sidebarMenu

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import {
+  Chat,
+  Dashboard,
+  Favorite,
+  LocalMovies,
+  Logout,
+  Stream,
+  TrendingUp,
+  VideogameAsset,
+} from "@mui/icons-material"
 import Head from 'next/head'
 import Header from '../Components/Header';
+import MenuItem from '../Components/MenuItem'
+
 
 const index = () => {
   return (
@@ -10,6 +22,27 @@ const index = () => {
             <link rel="icon" href="/favicon.ico" />
         </Head>
         <Header/>
+        <div className=' w-full h-[calc(100%-80px)]'>
+          {/* left menu */}
+          <div
+            id="leftMenu"
+            className="sm:w-[6%] md:w-[10%] min-w-[60px] h-full flex flex-col items-center justify-evenly">
+            <MenuItem icon={<Dashboard />} name={"Dashboard"} />
+            <MenuItem icon={<VideogameAsset />} name={"Games"} />
+            <MenuItem icon={<LocalMovies />} name={"Movies"} />
+            <MenuItem icon={<Stream />} name={"Streamer"} />
+            <MenuItem icon={<TrendingUp />} name={"Statistics"} />
+            <MenuItem icon={<Chat />} name={"Messages"} />
+            <MenuItem icon={<Favorite />} name={"Favourite"} />
+            <MenuItem icon={<Logout />} name={"Logout"} last={true} />
+          </div>
+          {/* ---------- */}
+          {/* Main menu */}
+          <div>
+
+          </div>
+          {/* ---------- */}
+        </div>
     </div>
   );
 };
